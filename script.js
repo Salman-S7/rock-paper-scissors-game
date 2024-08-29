f5e4dae188af5eb3777b662c2fbf51ba04f98a28
Salman-S7 <sammyzed1505@gmail.com>
2024-08-29 13:02:04 +0530

    removed console logs

--- script.js
+++ script.js
@@ -24,12 +24,9 @@ scissorButton.addEventListener("click", () => handleClick("SCISSOR"));
 
 //function to handle user selection
 function handleClick(userChoice) {
-  console.log("Users Choice is :" + userChoice);
-
   // get random choice of computer
   const randomIndex = Math.floor(Math.random() * 3);
   const computersChoice = computerSelectionArray[randomIndex];
-  console.log("Computer choice is : " + computersChoice);
 
   //updating dom
 
@@ -38,34 +35,22 @@ function handleClick(userChoice) {
 
   // check who have won
   if (userChoice === computersChoice) {
-    console.log("Draw");
+    //update dom to show DRAW
   } else if (
     (userChoice === "ROCK" && computersChoice === "SCISSORS") ||
     (userChoice === "SCISSOR" && computersChoice === "PAPER") ||
     (userChoice === "PAPER" && compChoicePara === "ROCK")
   ) {
-    console.log("User Won");
     userWinCount++;
     userWinCountPara.innerHTML = `Player: ${userWinCount}`;
   } else {
-    console.log("Computer won");
     computerWinCount++;
     compWinCountPara.innerHTML = `Computer: ${computerWinCount}`;
   }
 
   if (computerWinCount === 5) {
-    console.log(
-      "computer won the tournament UserWinCount is " +
-        userWinCount +
-        "and computers score is " +
-        computerWinCount
-    );
+    // update dom to show that computer has won and you lost
   } else if (userWinCount === 5) {
-    console.log(
-      "Congratulations you won .... UserWinCount is " +
-        userWinCount +
-        "and computers score is " +
-        computerWinCount
-    );
+    // update dom to show that you have won
   }
 }
